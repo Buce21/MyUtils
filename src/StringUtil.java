2afa951c0461dc7fe6d09fe02f9633a4a9933025
buce <378741605@qq.com>
2015-11-27 17:28:29 +0800

    更新

--- src/StringUtil.java
+++ src/StringUtil.java
@@ -26,8 +26,136 @@ public final class StringUtil {
      */
     public static boolean isNotEmpty(CharSequence cs) {
         boolean flag = false;
-        if (cs != null && cs.length() > 0 ) flag = true;
+        if( null == cs || cs.length() == 0 ) flag = true;
+        return !flag;
+    }
+
+    /**
+     * 判断字符数组中含有""字符串或者是null
+     * @param array
+     * @return 如果字符数组中含有""字符串或者是null返回true
+     */
+    public static boolean isAnyEmpty(CharSequence ... array) {
+        boolean flag = false;
+        for (CharSequence cs : array){
+            if ( null == cs || cs.length() == 0 ) {
+                flag = true;
+                break;
+            }
+        }
         return flag;
     }
+
+    /**
+     * 判断字符数组中不含有""字符串和null
+     * @param array
+     * @return 如果字符数组中不含有""字符串或者是null返回true
+     */
+    public static boolean isNoneEmpty(CharSequence ... array) {
+        boolean flag = false;
+        for (CharSequence cs : array){
+            if ( null == cs || cs.length() == 0 ) {
+                flag = true;
+                break;
+            }
+        }
+        return !flag;
+    }
+
+    /**
+     *判断内容是null或""的字符换或者空格字符串
+     * @param cs
+     * @return 如果内容是null或""的字符换或者空格字符串，返回false
+     */
+    public static boolean isBlank(CharSequence cs) {
+        boolean flag = false;
+        if (null == cs || cs.length() == 0 || isContainsKey(cs,' ')) {
+            flag = true;
+        }
+        return flag;
+    }
+
+    /**
+     * 判断字符串内容不是null或""或空格
+     * @param cs
+     * @return
+     */
+    public static boolean isNotBlank(CharSequence cs) {
+        boolean flag = false;
+        if (null == cs || cs.length() == 0 || isContainsKey(cs,' ')) {
+            flag = true;
+        }
+        return !flag;
+    }
+
+    /**
+     * 判断字符串数组中的元素是否为null或""或含有空格字符
+     * @param array
+     * @return
+     */
+    public static boolean isAnyBlank(CharSequence... array) {
+        boolean flag = false;
+        for (CharSequence cs : array) {
+            if (null == cs || cs.length() == 0 || isContainsKey(cs,' ')) {
+                flag = true;
+                break;
+            }
+        }
+        return flag;
+    }
+
+    /**
+     * 判断字符串数组中的所有元素都不含有null或者""
+     * @param array
+     * @return
+     */
+    public static boolean isNoneBlank(CharSequence... array) {
+        boolean flag = false;
+        for (CharSequence cs : array) {
+            if (null == cs || cs.length() == 0 || isContainsKey(cs,' ')) {
+                flag = true;
+                break;
+            }
+        }
+        return !flag;
+    }
+
+    /**
+     * 清除字符串中的空格字符，为null或者由空格字符组成的字符串返回null
+     * @param str
+     * @return
+     */
+    public static String trimToNull(String str) {
+        if (null == str || str.length() == 0) {
+            return null;
+        } else {
+            String str2 = str.replaceAll(" ","");
+            if (str2.length() > 0) {
+                return str2;
+            } else {
+                return null;
+            }
+        }
+    }
+    /**
+     * 判断cs中是都是key
+     * @param cs
+     * @param key
+     * @return 如果cs中是否都是key返回true
+     */
+    public static boolean isContainsKey(CharSequence cs,char key){
+        boolean flag = false;
+        int count = 0;
+        for (int i = 0; i < cs.length(); i ++) {
+            if (cs.charAt(i) == key) {
+                count ++;
+            }
+        }
+        if (count == cs.length()){
+            flag = true;
+        }
+        return flag;
+    }
+
 
 }
--- test/StringUtilTest.java
+++ test/StringUtilTest.java
@@ -1,27 +1,85 @@
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 /**
- * Created by Administrator on 2015/11/27.
+ * Created by Ce Bu on 2015/11/27.
  */
 public class StringUtilTest {
 
-    private static StringUtil stringUtil = new StringUtil();
-
+    StringUtil stringUtil ;
+    @Before
+    public void initStringUtil(){
+        stringUtil = new StringUtil();
+    }
     @Test
     public void isEmptyTest(){
-        assertEquals(stringUtil.isEmpty("a"),false);
-        assertEquals(stringUtil.isEmpty(null),true);
-        assertEquals(stringUtil.isEmpty(""),true);
+        assertEquals(StringUtil.isEmpty("a"),false);
+        assertEquals(StringUtil.isEmpty(null),true);
+        assertEquals(StringUtil.isEmpty(""),true);
     }
 
     @Test
     public void isNotEmptyTest() {
-        assertEquals(stringUtil.isNotEmpty("a"),true);
-        assertEquals(stringUtil.isNotEmpty(""),false);
-        assertEquals(stringUtil.isNotEmpty(null),false);
+        assertEquals(StringUtil.isNotEmpty("a"),true);
+        assertEquals(StringUtil.isNotEmpty(""),false);
+        assertEquals(StringUtil.isNotEmpty(null),false);
     }
 
+    @Test
+    public void isAnyEmptyTest() {
+        assertEquals(StringUtil.isAnyEmpty("rt","aw"),false);
+        assertEquals(StringUtil.isAnyEmpty("","rt"),true);
+        assertEquals(StringUtil.isAnyEmpty("rt",null),true);
+    }
 
+    @Test
+    public void isNoneEmptyTest() {
+        assertEquals(StringUtil.isNoneEmpty("rt","aw"),true);
+        assertEquals(StringUtil.isNoneEmpty("","rt"),false);
+        assertEquals(StringUtil.isNoneEmpty("rt",null),false);
+    }
+
+    @Test
+    public void isBlankTest() {
+        assertEquals(StringUtil.isBlank(" he "),false);
+        assertEquals(StringUtil.isBlank(null),true);
+        assertEquals(StringUtil.isBlank(""),true);
+        assertEquals(StringUtil.isBlank(" "),true);
+        assertEquals(StringUtil.isBlank("  "),true);
+    }
+
+    @Test
+    public void isNotBlankTest() {
+        assertEquals(StringUtil.isNotBlank(" "),false);
+        assertEquals(StringUtil.isNotBlank(null),false);
+        assertEquals(StringUtil.isNotBlank(""),false);
+        assertEquals(StringUtil.isNotBlank("a"),true);
+        assertEquals(StringUtil.isNotBlank(" a"),true);
+        assertEquals(StringUtil.isNotBlank("a "),true);
+    }
+
+    @Test
+    public void isAnyBlankTest() {
+        assertEquals(StringUtil.isAnyBlank(" "," we"),true);
+    }
+
+    @Test
+    public void isNoneBlankTest() {
+        assertEquals(StringUtil.isNoneBlank(" "," we"),false);
+    }
+
+    @Test
+    public void trimToNullTest() {
+        assertEquals(StringUtil.trimToNull(""),null);
+        assertEquals(StringUtil.trimToNull(null),null);
+        assertEquals(StringUtil.trimToNull(" "),null);
+        assertEquals(StringUtil.trimToNull("co me"),"come");
+    }
+    @After
+    public void closeStringUtil() {
+        stringUtil = null;
+    }
 }
